7996122e2a6025d21c824fa4a6cbc2bc9df5f6fc
Eshan D'Souza <eshandsouza@gmail.com>
2020-10-02 15:12:33 +0400

    Updated and appropriate constraints

--- app/src/main/java/com/example/getpet/AdoptFoster.java
+++ app/src/main/java/com/example/getpet/AdoptFoster.java
@@ -2,7 +2,11 @@ package com.example.getpet;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AdoptFoster extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class AdoptFoster extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adopt_foster);
+
+        adoptFoster();
+        findAHome();
     }
+
+    private void adoptFoster(){
+        Button adoptFoster = (Button) findViewById(R.id.adoptfoster);
+        adoptFoster.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdoptFoster.this,FilterBy.class));
+            }
+        });
+    }
+
+    private void findAHome(){
+        Button findahome = (Button) findViewById(R.id.findahome);
+        findahome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(AdoptFoster.this,FilterBy.class));
+            }
+        });
+    }
+
+
 }
--- app/src/main/java/com/example/getpet/FilterBy.java
+++ app/src/main/java/com/example/getpet/FilterBy.java
@@ -17,28 +17,28 @@ public class FilterBy extends AppCompatActivity implements AdapterView.OnItemSel
         setContentView(R.layout.activity_filter_by);
 
         Spinner spinner1 = findViewById(R.id.spinner1);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.types, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner1.setAdapter(adapter);
+        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this, R.array.type, android.R.layout.simple_spinner_item);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
         spinner1.setOnItemSelectedListener(this);
 
-//        Spinner spinner2 = findViewById(R.id.spinner2);
-//        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.types, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner2.setAdapter(adapter);
-//        spinner2.setOnItemSelectedListener(this);
-//
-//        Spinner spinner3 = findViewById(R.id.spinner3);
-//        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.types, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner3.setAdapter(adapter);
-//        spinner3.setOnItemSelectedListener(this);
-//
-//        Spinner spinner4 = findViewById(R.id.spinner4);
-//        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.types, android.R.layout.simple_spinner_item);
-//        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-//        spinner4.setAdapter(adapter);
-//        spinner4.setOnItemSelectedListener(this);
+        Spinner spinner2 = findViewById(R.id.spinner2);
+        ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(this, R.array.breed, android.R.layout.simple_spinner_item);
+        adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner2.setAdapter(adapter2);
+        spinner2.setOnItemSelectedListener(this);
+
+        Spinner spinner3 = findViewById(R.id.spinner3);
+        ArrayAdapter<CharSequence> adapter3 = ArrayAdapter.createFromResource(this, R.array.age, android.R.layout.simple_spinner_item);
+        adapter3.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner3.setAdapter(adapter3);
+        spinner3.setOnItemSelectedListener(this);
+
+        Spinner spinner4 = findViewById(R.id.spinner4);
+        ArrayAdapter<CharSequence> adapter4 = ArrayAdapter.createFromResource(this, R.array.adoptfoster, android.R.layout.simple_spinner_item);
+        adapter4.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner4.setAdapter(adapter4);
+        spinner4.setOnItemSelectedListener(this);
 
     }
 
