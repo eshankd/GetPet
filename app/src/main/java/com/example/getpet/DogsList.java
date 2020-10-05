906865d42872cbde5083a73328cb0381ec5e07be
Ben <benjaay28@hotmail.com>
2020-10-05 23:52:03 +0400

    OnBackPressed added for profile page DogList retrieval has to be fixed

--- app/src/main/java/com/example/getpet/DogsList.java
+++ app/src/main/java/com/example/getpet/DogsList.java
@@ -1,6 +1,7 @@
 package com.example.getpet;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -8,6 +9,8 @@ import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
@@ -15,20 +18,42 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class DogsList extends AppCompatActivity {
 
-//    TextView  name,age,gender,breed;
-//    Button btn;
-//    DatabaseReference reff;
-BottomNavigationView navBar;
+    BottomNavigationView navBar;
+
+    private FirebaseFirestore fStore;
+
+    private String name;
+    private String age;
+    private String breed;
+    private String gender;
+
+    private TextView nameOUT;
+    private TextView ageOUT;
+    private TextView breedOUT;
+    private TextView genderOUT;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dogs_list);
 
+        fStore = FirebaseFirestore.getInstance();
+
         navBar = findViewById(R.id.bottom_navbar);
+
+        nameOUT = findViewById(R.id.dogName);
+        ageOUT = findViewById(R.id.dogAge);
+        breedOUT = findViewById(R.id.dogBreed);
+        genderOUT = findViewById(R.id.dogGender);
 
         navBar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -54,37 +79,35 @@ BottomNavigationView navBar;
             }
         });
 
+        loadDogs();
 
-//        name=findViewById(R.id.name);
-//        age=findViewById(R.id.age);
-//        gender=findViewById(R.id.gender);
-//        breed=findViewById(R.id.breed);
-//        btn=findViewById(R.id.btnload);
-//
-//       btn.setOnClickListener(new View.OnClickListener(){
-//
-//           public void onClick(View view){
-//               reff= FirebaseDatabase.getInstance()
-//                       .getReference()
-//                       .child("Member")
-//                       .child("1");
-//               reff.addValueEventListener(new ValueEventListener() {
-//                   @Override
-//                   public void onDataChange(@NonNull DataSnapshot snapshot) {
-//
-//                       String name = dataSnapshot.child("name").getValue().toString();
-//                   }
-//
-//                   @Override
-//                   public void onCancelled(@NonNull DatabaseError error) {
-//
-//                   }
-//               });
-//
-//
-//                                  }
-//       });
+    }
 
+    private void loadDogs(){
+        Button loadDogsBTN = findViewById(R.id.btnload);
+        loadDogsBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fStore.collection("Dogs").addSnapshotListener(new EventListener<QuerySnapshot>(){
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        for (QueryDocumentSnapshot doc : value){
+                            name = doc.getString("Name");
+                            age = doc.getString("Age");
+                            breed = doc.getString("Breed");
+                            gender = doc.getString("Gender");
 
+                            nameOUT.setText(name);
+                            ageOUT.setText(age);
+                            breedOUT.setText(breed);
+                            genderOUT.setText(gender);
+
+                            break;
+                        }
+
+                    }
+                });
+            }
+        });
     }
 }
--- app/src/main/java/com/example/getpet/Profile.java
+++ app/src/main/java/com/example/getpet/Profile.java
@@ -6,17 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Profile extends AppCompatActivity {
 
     BottomNavigationView navBar;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        auth = FirebaseAuth.getInstance();
 
         navBar = findViewById(R.id.bottom_navbar);
         navBar.setSelectedItemId((R.id.profile));
@@ -42,6 +49,20 @@ public class Profile extends AppCompatActivity {
                         break;
                 }
                 return true;
+            }
+        });
+
+        signOut();
+    }
+
+    private void signOut(){
+        Button signOutBTN = findViewById(R.id.signOut);
+        signOutBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.signOut();
+                startActivity(new Intent(Profile.this, MainActivity.class));
+                Toast.makeText(Profile.this, "Successfully signed out", Toast.LENGTH_SHORT).show();
             }
         });
     }
