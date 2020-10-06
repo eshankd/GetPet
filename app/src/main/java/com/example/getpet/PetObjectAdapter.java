3118120ea60c447bee325c95cd9e2a19c8cfaf60
ethanjd <ethandsouza2@gmail.com>
2020-10-07 01:39:02 +0400

    Fixed listView in DogsList Array adapter is working in DogsList dog_list_layout.xml finished

--- app/src/main/java/com/example/getpet/PetObjectAdapter.java
+++ app/src/main/java/com/example/getpet/PetObjectAdapter.java
@@ -15,19 +15,20 @@ import java.util.List;
 
 public class PetObjectAdapter extends ArrayAdapter<PetObject> {
     private Context mContext;
-    private List<PetObject> petObjectList = new ArrayList<>();
+    private List<PetObject> petObjectList;
+
     public PetObjectAdapter(Context context, ArrayList<PetObject> list) {
         super(context, 0, list);
         mContext = context;
         petObjectList = list;
-
     }
 
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View listItem = convertView;
-        if(petObjectList == null)
+
+        if(listItem == null)
             listItem = LayoutInflater.from(mContext).inflate(R.layout.dog_list_layout, parent, false);
 
         PetObject currentPet = petObjectList.get(position);
@@ -36,7 +37,7 @@ public class PetObjectAdapter extends ArrayAdapter<PetObject> {
         name.setText(currentPet.getName());
 
         TextView age = listItem.findViewById(R.id.petAge);
-        age.setText(currentPet.getAge());
+        age.setText(Integer.toString(currentPet.getAge()));
 
         TextView gender = listItem.findViewById(R.id.petGender);
         gender.setText(currentPet.getGender());
