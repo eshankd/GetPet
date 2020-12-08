13284f70ea5b8f9960b953476474a50df31934bb
Ben <benjaay28@hotmail.com>
2020-12-08 20:49:59 +0400

    User Profile Pictures now added 2 new users added

--- app/src/main/java/com/example/getpet/Profile.java
+++ app/src/main/java/com/example/getpet/Profile.java
@@ -1,11 +1,19 @@
 package com.example.getpet;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -13,15 +21,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FieldPath;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -30,16 +48,39 @@ public class Profile extends AppCompatActivity {
     BottomNavigationView navBar;
     private FirebaseAuth auth;
     private FirebaseFirestore fStore;
+    private FirebaseStorage storage;
+    private StorageReference storageRef;
+    private StorageReference userPicRef;
     private User user;
     private TextView nameOUT;
     private TextView emailOUT;
     private TextView petsOwnedOUT;
     private ImageView profilePictureOut;
+    private Bitmap bitmap;
+    private byte[] byteArray;
+    private String TAG = "Profile";
 
     private TextView petsOwnedLabel;
     private TextView emailLabel;
 
     private Button loginButton;
+    private Button editUserPicBtn;
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK){
+                        Intent data = result.getData();
+                        try {
+                            openFileChosen(data);
+                        } catch (FileNotFoundException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,8 +89,12 @@ public class Profile extends AppCompatActivity {
 
         user = User.getInstance();
         auth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+        storageRef = storage.getReference();
+        userPicRef = storageRef.child("User Images/" + user.getUserID() + ".jpg");
 
         loginButton = findViewById(R.id.loginButton);
+        editUserPicBtn = findViewById(R.id.chooseUserPic);
 
         navBar = findViewById(R.id.bottom_navbar);
         navBar.setSelectedItemId((R.id.profile));
@@ -92,11 +137,71 @@ public class Profile extends AppCompatActivity {
             emailOUT.setText(user.getEmail());
             petsOwnedOUT.setText(Integer.toString(user.getPetsOwned()));
             loginButton.setVisibility(View.GONE);
+
+
+
+            final File localFile;
+            try {
+                localFile = File.createTempFile(user.getUserID(), "jpg");
+                userPicRef.getFile(localFile).addOnSuccessListener(taskSnapshot -> {
+                    Bitmap bitmap = BitmapFactory.decodeFile(localFile.getAbsolutePath());
+                    profilePictureOut.setImageBitmap(bitmap);
+                });
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
 
         signOut();
         loginButton();
+        editUserPicButton();
     }
+
+    private void editUserPicButton() {
+        editUserPicBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent getIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                getIntent.setType("image/*");
+
+                Intent pickIntent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                pickIntent.setType("image/*");
+
+                Intent chooserIntent = Intent.createChooser(getIntent, "Select Image");
+                chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Intent[] {pickIntent});
+
+                someActivityResultLauncher.launch(chooserIntent);
+            }
+        });
+    }
+
+    private void openFileChosen(Intent data) throws FileNotFoundException {
+
+        InputStream inputStream = getContentResolver().openInputStream(data.getData());
+        bitmap = BitmapFactory.decodeStream(inputStream);
+        profilePictureOut.setImageBitmap(bitmap);
+
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+        byteArray = stream.toByteArray();
+
+//        picUp = true;
+
+        UploadTask uploadTask = userPicRef.putBytes(byteArray);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d(TAG, "Picture not uploaded");
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                Log.d(TAG, "Picture uploaded");
+            }
+        });
+    }
+
 
     private void signOut() {
         Button signOutBTN = findViewById(R.id.signOut);
